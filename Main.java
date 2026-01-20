1937ccd4b6fe9956f6dbb2303e59e42310e86962
Patopm <egs.pato.pm@gmail.com>
2026-01-20 11:44:30 -0600

    refactor: simplifies code for reusability

--- Main.java
+++ Main.java
@@ -6,52 +6,57 @@ public class Main {
   private static final Scanner sc = new Scanner(System.in);
 
   public static void main(String[] args) {
-    String nombre;
-    try {
-      System.out.print("Ingresa tu nombre: ");
-      nombre = sc.next();
-      switch (nombre.toLowerCase()) {
-        case "pato":
-          System.out.println("Quak");
-          break;
-        case "":
-          System.out.println("Se supone que tienes que poner un nombre .-.");
-          break;
-        default:
-          System.out.println("Hola " + nombre);
-          break;
-      }
-    } catch (Exception e) {
-      System.out.println(e);
-    }
+    String nombre = pedirNombre();
+    saludar(nombre);
 
-    System.out.println("Aprendi a contar en pares, quieres ver? (y/n)");
-    char decision;
-    try {
-      decision = (char) sc.next().charAt(0);
-      switch (decision) {
-        case 'y':
-          System.out.println("Vaa, preparate");
-          List<Integer> pares = new ArrayList<Integer>();
-          for (int i = 2; i <= 100; i = i + 2) {
-            pares.add(i);
-          }
-          for (int par : pares) {
-            System.out.println(par);
-          }
-          System.out.println("Listoo, gracias por tu interes, ahora bye");
-          break;
-        case 'n':
-          System.out.println("Ash, que aburrido, ya vete mejor");
-          break;
-        default:
-          System.out.println("Si estas poniendo atención? Ya mejor alejate de mi vista");
-          break;
-      }
-    } catch (Exception e) {
-      System.out.println(e);
+    if (preguntarSiContarPares()) {
+      contarPares();
     }
 
     sc.close();
+  }
+
+  private static String pedirNombre() {
+    System.out.print("Ingresa tu nombre: ");
+    return sc.next();
+  }
+
+  private static void saludar(String nombre) {
+    switch (nombre.toLowerCase()) {
+      case "pato":
+        System.out.println("Quak");
+        break;
+      case "":
+        System.out.println("Se supone que tienes que poner un nombre .-.");
+        break;
+      default:
+        System.out.println("Hola " + nombre);
+        break;
+    }
+  }
+
+  private static boolean preguntarSiContarPares() {
+    System.out.println("Aprendi a contar en pares, quieres ver? (y/n)");
+    char decision = sc.next().charAt(0);
+    switch (decision) {
+      case 'y':
+        System.out.println("Vaa, preparate");
+        return true;
+      case 'n':
+        System.out.println("Ash, que aburrido, ya vete mejor");
+        return false;
+      default:
+        System.out.println("Si estas poniendo atención? Ya mejor alejate de mi vista");
+        return false;
+    }
+  }
+
+  private static void contarPares() {
+    List<Integer> pares = new ArrayList<>();
+    for (int i = 2; i <= 100; i += 2) {
+      pares.add(i);
+    }
+    pares.forEach(System.out::println);
+    System.out.println("Listoo, gracias por tu interes, ahora bye");
   }
 }
